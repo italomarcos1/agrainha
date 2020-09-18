5e64e4c5ee971c7020bda7bb101753b018026d8c
Italo M <italomarcos0010@gmail.com>
2020-09-18 13:16:22 -0300

    another commit

--- src/components/Product/styles.js
+++ src/components/Product/styles.js
@@ -26,7 +26,7 @@ export const Container = styled.div`
       padding: 0 5px;
       margin: 10px 0;
       height: 70px;
-      width: 45.82%;
+      width: 100%;
 
       overflow: hidden;
       /* white-space: pre; */
