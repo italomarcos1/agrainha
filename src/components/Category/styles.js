203a6e90efe66704d898af840c2a012bdccc6f86
Italo M <italomarcos0010@gmail.com>
2020-09-19 10:31:42 -0300

    another commit

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   justify-content: center;
   border-radius: 12px;
   padding: 10px 5px;
-  width: 33%;
+  width: 31%;
   height: 100px;
   position: relative;
 `;
