e188a9558d0ffc93c7431e13d48025ef082c2256
Italo M <italomarcos0010@gmail.com>
2020-09-19 14:01:12 -0300

    another commit

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -38,8 +38,8 @@ export default function Product({ match }) {
 
   // const [isFavorite, setIsFavorite] = useState(product.isFavorite);
   const [amount, setAmount] = useState(0);
-  window.scrollTo(-1000, 0);
-  ref.current.scrollTo(-1000, 0);
+  window.scrollTo(0, 0);
+  ref.current.scrollTo(0, 0);
 
   useEffect(() => {
     const productsGridRows = Math.ceil(data.length / 2);
