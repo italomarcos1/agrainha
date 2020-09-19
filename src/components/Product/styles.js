92e82dae5c8978d716d83a4248a809ac3713bb2d
Italo M <italomarcos0010@gmail.com>
2020-09-19 10:00:12 -0300

    another commit

--- src/components/Product/styles.js
+++ src/components/Product/styles.js
@@ -104,7 +104,7 @@ export const Options = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    width: 60%;
+    width: 65%;
     height: 100%;
     /* margin-right: 12px; */
 
