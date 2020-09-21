3e68d02b859af673047d023a00706f8278374d36
Italo M <italomarcos0010@gmail.com>
2020-09-21 13:51:06 -0300

    another commit

--- src/pages/Basket/styles.js
+++ src/pages/Basket/styles.js
@@ -50,7 +50,7 @@ export const ProductInfo = styled.div`
   height: 70px;
   justify-content: space-between;
   align-items: flex-start;
-  width: 80%;
+  width: 75%;
 `;
 
 export const Price = styled.strong`
